5c42c89436f7ee1a7f43107245d4ec98f4121381
Beka Chkhaidze <56247764+chkhaidzebeka@users.noreply.github.com>
2023-09-03 16:24:15 +0400

    Delete js/instaAnonymousStoryViewer.js

--- js/instaAnonymousStoryViewer.js
+++
@@ -1,7 +0,0 @@
-// 1. go to instagram profile and open network tab
-// 2. filter to only XHR request
-// 3. find /story and copy whole response
-
-
-const instaAnonymousStoryViewer = storyOBj => 
-  storyOBj.reel.items.map(el => (el.video_versions || el.image_versions2.candidates)[0].url)
