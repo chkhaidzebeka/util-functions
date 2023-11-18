cd697bc1c8511343d6816785dfa175d04382b8a8
Beka Chkhaidze <56247764+chkhaidzebeka@users.noreply.github.com>
2023-11-18 14:00:50 +0400

    added Prettify

--- ts/HelperTypes.ts
+++ ts/HelperTypes.ts
@@ -1,2 +1,7 @@
 type LooseAutocomplete<T extends string> = T | Omit<string, T>;
 // type IconType = LooseAutocomplete<"sm" | "xs">;
+
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+// type ComplexObj = Prettify<Type1 & Type2 & ...TypeN>
