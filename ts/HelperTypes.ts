8bf7033ed0cb4c6bdc1dc3199b13835413f24b72
Beka Chkhaidze <56247764+chkhaidzebeka@users.noreply.github.com>
2024-01-07 00:12:42 +0400

    Update HelperTypes.ts

--- ts/HelperTypes.ts
+++ ts/HelperTypes.ts
@@ -5,3 +5,7 @@ type Prettify<T> = {
   [K in keyof T]: T[K];
 } & {};
 // type ComplexObj = Prettify<Type1 & Type2 & ...TypeN>
+
+type IsEqual<X, Y> = 
+  (<T>() => T extends X ? 1 : 2) extends 
+	(<T>() => T extends Y ? 1 : 2) ? true : false;
